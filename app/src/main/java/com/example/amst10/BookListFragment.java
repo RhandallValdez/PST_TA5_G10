d840adcf6386ea373c2fd5bde2227c7d3b27515a
fernando <fdtroya@espol.edu.ec>
2020-08-14 22:25:46 -0500

    solucion de errores

--- app/src/main/java/com/example/amst10/BookListFragment.java
+++ app/src/main/java/com/example/amst10/BookListFragment.java
@@ -13,8 +13,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class BookListFragment extends Fragment {
     private  Activity activity;
+    public static ArrayList<Book> items=new ArrayList<>();
+
+
+
     public BookListFragment(Activity activity){
        super();
         this.activity=activity;
@@ -22,17 +28,17 @@ public class BookListFragment extends Fragment {
     public BookListFragment(){
         super();
     }
+    public static  void  addBooks(){
+        items.add(new Book("https://static.megustaleer.com/images/libros_650_x/P32147B.jpg","Ray Bradbury","Ballantine Books","Fahrenheit 451","Guy Montag es un bombero y el trabajo de un bombero es quemar libros, que están prohibidos porque son causa de discordia y sufrimiento.","Ciencia Ficcion"));
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         RecyclerView recyclerView;
         View v= inflater.inflate(R.layout.fragment_list,container,false);
-       final BookAdapter adapter=new BookAdapter(getContext(),****items,activity);//items es el arrayList
-        /*recyclerView=v.findViewById(R.id.listBooks);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        */
+       final BookAdapter adapter=new BookAdapter(getContext(),items,activity);//items es el arrayList
+
 
         SearchView sv=v.findViewById(R.id.searchBooks);
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
